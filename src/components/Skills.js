5f941eda97e3a7cb0a631fd93f01ddc5fff66918
dishanth <shettysamarth738@gmail.com>
2023-11-21 16:00:12 +0530

    added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ function Skills() {
     </div>
     
     <div className="first">
-    <div className="lang">
+    <div className="lang hov">
         <h2>Languages</h2>
         <h4>C</h4>
         <h4>C++</h4>
@@ -17,7 +17,7 @@ function Skills() {
         <h4>CSS 3</h4>
         <h4>JavaScript</h4>
       </div>
-      <div className='backend'>
+      <div className='backend hov'>
         <h2>Backend Development</h2>
         <h4>NODE JS</h4>
         <h4>EXPRESS JS</h4>
@@ -25,14 +25,14 @@ function Skills() {
 
     </div>
       <div className="second">
-      <div className='frontend'>
+      <div className='frontend hov'>
      <h2>Frontend Development:</h2> 
      <h4>REACT JS</h4>
      <h4>vue js</h4>
      <h4>ANGULAR JS</h4>
 
       </div>
-      <div className="database">
+      <div className="database hov">
         <h2>DATABASE</h2>
         <h4>MONGO DB</h4>
         <h4>SQL</h4>
